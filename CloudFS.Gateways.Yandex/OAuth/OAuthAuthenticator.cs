82ac620e3517bb96a73ab31f3a6efa9edfd36c45
viciousviper <j_h@mail.org>
2016-01-29 22:35:57 +0100

    Added gateway for Yandex Disk - Presently 9 of 18 online tests fail when executed in sequence with errors either "Resource is locked" or "Target directory is existent". - 8 of these will pass when executed individually. - The one test persistently failing is RemoveItemAsync_WhereItemIsFile_ExecutesRemove - it fails with a NullReferenceException when trying to wait for the Link returned from the DeleteAsync() method.

---
+++ CloudFS.Gateways.Yandex/OAuth/OAuthAuthenticator.cs
@@ -0,0 +1,116 @@
+/*
+The MIT License(MIT)
+
+Copyright(c) 2015 IgorSoft
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+using System;
+using System.Globalization;
+using System.Text;
+using System.Threading.Tasks;
+using YandexDisk.Client;
+using YandexDisk.Client.Http;
+using IgorSoft.CloudFS.Authentication;
+
+namespace IgorSoft.CloudFS.Gateways.Yandex.OAuth
+{
+    internal static class OAuthAuthenticator
+    {
+        private const string YANDEX_LOGIN_AUTHORIZE_URI = "https://oauth.yandex.com/authorize";
+
+        private const string YANDEX_LOGIN_TOKEN_URI = "https://oauth.yandex.com/verification_code";
+
+        private static string LoadRefreshToken(string account)
+        {
+            var refreshTokens = Settings.Default.RefreshTokens;
+            if (refreshTokens != null)
+                foreach (RefreshTokenSetting setting in refreshTokens)
+                    if (setting.Account == account)
+                        return setting.Token;
+
+            return  null;
+        }
+
+        private static void SaveRefreshToken(string account, string refreshToken)
+        {
+            var refreshTokens = Settings.Default.RefreshTokens;
+            if (refreshTokens != null) {
+                foreach (RefreshTokenSetting setting in refreshTokens)
+                    if (setting.Account == account) {
+                        refreshTokens.Remove(setting);
+                        break;
+                    }
+            } else {
+                refreshTokens = Settings.Default.RefreshTokens = new System.Collections.ObjectModel.Collection<RefreshTokenSetting>();
+            }
+
+            refreshTokens.Insert(0, new RefreshTokenSetting() { Account = account, Token = refreshToken });
+
+            Settings.Default.Save();
+        }
+
+        private static Uri GetAuthenticationUri(string clientId)
+        {
+            var stringBuilder = new StringBuilder()
+                .Append($"client_id={clientId}")
+                .Append("&response_type=token")
+                .Append("&display=popup");
+
+            return new UriBuilder(YANDEX_LOGIN_AUTHORIZE_URI) { Query = stringBuilder.ToString() }.Uri;
+        }
+
+        private static string GetAuthCode(string account, Uri authenticationUri, Uri redirectUri)
+        {
+            string oauth_token = null;
+
+            var browser = new BrowserLogOn();
+            browser.Authenticated += (s, e) => {
+                oauth_token = e.Parameters["access_token"];
+                browser.Close();
+            };
+            browser.Show("Yandex", account, authenticationUri, redirectUri);
+
+            return oauth_token;
+        }
+
+        public static Task<IDiskApi> Login(string account, string code)
+        {
+            if (string.IsNullOrEmpty(account))
+                throw new ArgumentNullException(nameof(account));
+
+            var refreshToken = LoadRefreshToken(account);
+
+            if (string.IsNullOrEmpty(refreshToken) && string.IsNullOrEmpty(code)) {
+                var authenticationUri = GetAuthenticationUri(Secrets.CLIENT_ID);
+                code = GetAuthCode(account, authenticationUri, new Uri(YANDEX_LOGIN_TOKEN_URI));
+
+                if (string.IsNullOrEmpty(code))
+                    throw new InvalidOperationException(string.Format(CultureInfo.CurrentCulture, Resources.RetrieveAuthenticationCodeFromUri, authenticationUri.ToString()));
+
+                refreshToken = code;
+            }
+
+            SaveRefreshToken(account, refreshToken);
+
+            return Task.FromResult<IDiskApi>(new DiskHttpApi(refreshToken));
+        }
+    }
+}
---
+++ CloudFS.Gateways.Yandex/YandexGateway.cs
@@ -0,0 +1,242 @@
+﻿/*
+The MIT License(MIT)
+
+Copyright(c) 2015 IgorSoft
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Composition;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using YandexDisk.Client;
+using YandexDisk.Client.Protocol;
+using IgorSoft.CloudFS.Gateways.Yandex.OAuth;
+using IgorSoft.CloudFS.Interface;
+using IgorSoft.CloudFS.Interface.Composition;
+using IgorSoft.CloudFS.Interface.IO;
+
+namespace IgorSoft.CloudFS.Gateways.Yandex
+{
+    [ExportAsAsyncCloudGateway("Yandex")]
+    [ExportMetadata(nameof(CloudGatewayMetadata.CloudService), YandexGateway.SCHEMA)]
+    [ExportMetadata(nameof(CloudGatewayMetadata.ServiceUri), YandexGateway.URL)]
+    [ExportMetadata(nameof(CloudGatewayMetadata.ApiAssembly), nameof(YandexDisk))]
+    [System.Diagnostics.DebuggerDisplay("{DebuggerDisplay(),nq}")]
+    public sealed class YandexGateway : IAsyncCloudGateway
+    {
+        private const string SCHEMA = "yandex";
+
+        private const string URL = "https://www.yandex.com";
+
+        private const int RETRIES = 3;
+
+        private class YandexContext
+        {
+            public IDiskApi Client { get; }
+
+            public YandexContext(IDiskApi client)
+            {
+                Client = client;
+            }
+        }
+
+        private IDictionary<RootName, YandexContext> contextCache = new Dictionary<RootName, YandexContext>();
+
+        public bool PreservesId => false;
+
+        private async Task<YandexContext> RequireContext(RootName root, string apiKey = null)
+        {
+            if (root == null)
+                throw new ArgumentNullException(nameof(root));
+
+            var result = default(YandexContext);
+            if (!contextCache.TryGetValue(root, out result)) {
+                var client = await OAuthAuthenticator.Login(root.UserName, apiKey);
+                contextCache.Add(root, result = new YandexContext(client));
+            }
+            return result;
+        }
+
+        private async Task<bool> OperationProgress(YandexContext context, Link link)
+        {
+            var operation = default(Operation);
+            do {
+                operation = await context.Client.Commands.GetOperationStatus(link, CancellationToken.None);
+                if (operation.Status == OperationStatus.Failure)
+                    return false;
+            } while (operation.Status == OperationStatus.InProgress);
+            return true;
+        }
+
+        public async Task<DriveInfoContract> GetDriveAsync(RootName root, string apiKey, IDictionary<string, string> parameters)
+        {
+            var context = await RequireContext(root, apiKey);
+
+            //var item = await AsyncFunc.Retry<Disk, YandexApiException>(async () => await context.Client.MetaInfo.GetDiskInfoAsync(CancellationToken.None), RETRIES);
+            var item = await context.Client.MetaInfo.GetDiskInfoAsync(CancellationToken.None);
+
+            return new DriveInfoContract(root.Value, item.TotalSpace - item.UsedSpace - item.TrashSize, item.UsedSpace);
+        }
+
+        public async Task<RootDirectoryInfoContract> GetRootAsync(RootName root, string apiKey)
+        {
+            var context = await RequireContext(root, apiKey);
+
+            var request = new ResourceRequest() { Path = "/" };
+            //var item = await AsyncFunc.Retry<Resource, YandexApiException>(async () => await context.Client.MetaInfo.GetInfoAsync(request, CancellationToken.None), RETRIES);
+            var item = await context.Client.MetaInfo.GetInfoAsync(request, CancellationToken.None);
+
+            return new RootDirectoryInfoContract(item.Path, item.Created, item.Modified);
+        }
+
+        public async Task<IEnumerable<FileSystemInfoContract>> GetChildItemAsync(RootName root, DirectoryId parent)
+        {
+            var context = await RequireContext(root);
+
+            var request = new ResourceRequest() { Path = parent.Value };
+            //var item = await AsyncFunc.Retry<Resource, YandexApiException>(async () => await context.Client.MetaInfo.GetInfoAsync(request, CancellationToken.None), RETRIES);
+            var item = await context.Client.MetaInfo.GetInfoAsync(request, CancellationToken.None);
+
+            return item.Embedded.Items.Select(i => i.ToFileSystemInfoContract());
+        }
+
+        public async Task<bool> ClearContentAsync(RootName root, FileId target, Func<FileSystemInfoLocator> locatorResolver)
+        {
+            var context = await RequireContext(root);
+
+            var link = await context.Client.Files.GetUploadLinkAsync(target.Value, true, CancellationToken.None);
+            await context.Client.Files.UploadAsync(link, new MemoryStream(), CancellationToken.None);
+
+            return true;
+        }
+
+        public async Task<Stream> GetContentAsync(RootName root, FileId source)
+        {
+            var context = await RequireContext(root);
+
+            var link = await context.Client.Files.GetDownloadLinkAsync(source.Value, CancellationToken.None);
+            var stream = await context.Client.Files.DownloadAsync(link, CancellationToken.None);
+
+            return stream;
+        }
+
+        public async Task<bool> SetContentAsync(RootName root, FileId target, Stream content, IProgress<ProgressValue> progress, Func<FileSystemInfoLocator> locatorResolver)
+        {
+            var context = await RequireContext(root);
+
+            var link = await context.Client.Files.GetUploadLinkAsync(target.Value, true, CancellationToken.None);
+            await context.Client.Files.UploadAsync(link, content, CancellationToken.None);
+
+            return true;
+        }
+
+        public async Task<FileSystemInfoContract> CopyItemAsync(RootName root, FileSystemId source, string copyName, DirectoryId destination, bool recurse)
+        {
+            var context = await RequireContext(root);
+
+            var path = !string.IsNullOrEmpty(copyName) ? destination.Value.TrimEnd('/') + '/' + copyName : destination.Value;
+            var copyRequest = new CopyFileRequest() { From = source.Value, Path = path };
+            var link = await context.Client.Commands.CopyAsync(copyRequest, CancellationToken.None);
+            if (!await OperationProgress(context, link))
+                throw new ApplicationException(string.Format(CultureInfo.CurrentCulture, Resources.OperationFailed, nameof(YandexDisk.Client.Clients.ICommandsClient.CopyAsync)));
+            var request = new ResourceRequest() { Path = path };
+            var item = await context.Client.MetaInfo.GetInfoAsync(request, CancellationToken.None);
+
+            return item.ToFileSystemInfoContract();
+        }
+
+        public async Task<FileSystemInfoContract> MoveItemAsync(RootName root, FileSystemId source, string moveName, DirectoryId destination, Func<FileSystemInfoLocator> locatorResolver)
+        {
+            var context = await RequireContext(root);
+
+            var path = !string.IsNullOrEmpty(moveName) ? destination.Value.TrimEnd('/') + '/' + moveName : destination.Value;
+            var moveRequest = new MoveFileRequest() { From = source.Value, Path = path };
+            var link = await context.Client.Commands.MoveAsync(moveRequest, CancellationToken.None);
+            if (!await OperationProgress(context, link))
+                throw new ApplicationException(string.Format(CultureInfo.CurrentCulture, Resources.OperationFailed, nameof(YandexDisk.Client.Clients.ICommandsClient.MoveAsync)));
+            var request = new ResourceRequest() { Path = path };
+            var item = await context.Client.MetaInfo.GetInfoAsync(request, CancellationToken.None);
+
+            return item.ToFileSystemInfoContract();
+        }
+
+        public async Task<DirectoryInfoContract> NewDirectoryItemAsync(RootName root, DirectoryId parent, string name)
+        {
+            var context = await RequireContext(root);
+
+            var request = new ResourceRequest() { Path = parent.Value.TrimEnd('/') + '/' + name };
+            var link = await context.Client.Commands.CreateDictionaryAsync(request.Path, CancellationToken.None);
+            if (!await OperationProgress(context, link))
+                throw new ApplicationException(string.Format(CultureInfo.CurrentCulture, Resources.OperationFailed, nameof(YandexDisk.Client.Clients.ICommandsClient.CreateDictionaryAsync)));
+            var item = await context.Client.MetaInfo.GetInfoAsync(request, CancellationToken.None);
+
+            return new DirectoryInfoContract(item.Path, item.Name, item.Created, item.Modified);
+        }
+
+        public async Task<FileInfoContract> NewFileItemAsync(RootName root, DirectoryId parent, string name, Stream content, IProgress<ProgressValue> progress)
+        {
+            var context = await RequireContext(root);
+
+            var request = new ResourceRequest() { Path = parent.Value.TrimEnd('/') + '/' + name };
+            var link = await context.Client.Files.GetUploadLinkAsync(request.Path, false, CancellationToken.None);
+            await context.Client.Files.UploadAsync(link, content, CancellationToken.None);
+            var item = await context.Client.MetaInfo.GetInfoAsync(request, CancellationToken.None);
+
+            return new FileInfoContract(item.Path, item.Name, item.Created, item.Modified, item.Size, item.Md5);
+        }
+
+        public async Task<bool> RemoveItemAsync(RootName root, FileSystemId target, bool recurse)
+        {
+            var context = await RequireContext(root);
+
+            var request = new DeleteFileRequest() { Path = target.Value };
+            var link = await context.Client.Commands.DeleteAsync(request, CancellationToken.None);
+            if (!await OperationProgress(context, link))
+                throw new ApplicationException(string.Format(CultureInfo.CurrentCulture, Resources.OperationFailed, nameof(YandexDisk.Client.Clients.ICommandsClient.DeleteAsync)));
+
+            return true;
+        }
+
+        public async Task<FileSystemInfoContract> RenameItemAsync(RootName root, FileSystemId target, string newName, Func<FileSystemInfoLocator> locatorResolver)
+        {
+            var context = await RequireContext(root);
+
+            var indexOfName = target.Value.LastIndexOf('/');
+            var path = target.Value.Substring(0, indexOfName + 1) + newName;
+            var moveRequest = new MoveFileRequest() { From = target.Value, Path = path };
+            var link = await context.Client.Commands.MoveAsync(moveRequest, CancellationToken.None);
+            if (!await OperationProgress(context, link))
+                throw new ApplicationException(string.Format(CultureInfo.CurrentCulture, Resources.OperationFailed, nameof(YandexDisk.Client.Clients.ICommandsClient.MoveAsync)));
+            var request = new ResourceRequest() { Path = path };
+            var item = await context.Client.MetaInfo.GetInfoAsync(request, CancellationToken.None);
+
+            return item.ToFileSystemInfoContract();
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode", Justification = "Debugger Display")]
+        [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+        private static string DebuggerDisplay() => nameof(YandexGateway);
+    }
+}
