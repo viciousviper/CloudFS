e0e3f753c6c195b8db3dac143d46321b5457919c
viciousviper <j_h@mail.org>
2016-02-01 20:17:55 +0100

    Included locally built YandexDisk.Client.dll

--- SignAssembly.cmd
+++ SignAssembly.cmd
@@ -1,5 +1,5 @@
 echo Signing assembly "%~2\%~1".dll
 @"%ProgramFiles(x86)%\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6.1 Tools\x64\ildasm" "%~2\%~1".dll /out:"%~3\%~1".il
-@"%Windir%\Microsoft.NET\Framework64\v4.0.30319\ilasm" "%~3\%~1".il /res:"%~3\%~1".res /dll /opt /key:..\..\..\..\..\IgorSoft\IgorSoft.OpenSource.snk /out:"%~3\%~1".dll /quiet
+@"%Windir%\Microsoft.NET\Framework64\v4.0.30319\ilasm" "%~3\%~1".il /res:"%~3\%~1".res /dll /opt /key:..\..\IgorSoft\IgorSoft.OpenSource.snk /out:"%~3\%~1".dll /quiet
 @del %3\%1.il
 @del %3\%1.res
