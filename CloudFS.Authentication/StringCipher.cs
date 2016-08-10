e4c7985ac0ddcc60c26acd32eb13905e8e38c01c
viciousviper <j_h@mail.org>
2016-08-11 01:43:47 +0200

    Added string encryption class

---
+++ CloudFS.Authentication/StringCipher.cs
@@ -0,0 +1,127 @@
+﻿/*
+The MIT License(MIT)
+
+Copyright(c) 2016 IgorSoft
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace IgorSoft.CloudFS.Authentication
+{
+    /// <summary>
+    /// Encrypts and decrypts <see cref="string"/> content with the <see cref="RijndaelManaged"/> algorithm.
+    /// </summary>
+    public static class StringCipher
+    {
+        // This constant is used to determine the keysize of the encryption algorithm in bits.
+        // We divide this by 8 within the code below to get the equivalent number of bytes.
+        private const int Keysize = 256;
+
+        // This constant is used to determine the blocksize of the encryption algorithm in bits.
+        private const int BlockSize = 256;
+
+        // This constant determines the number of iterations for the password bytes generation function.
+        private const int DerivationIterations = 1000;
+
+        /// <summary>Encrypts the specified plain text.</summary>
+        /// <param name="plainText">The plain text.</param>
+        /// <param name="passPhrase">The pass phrase.</param>
+        /// <returns>The cipher text.</returns>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2202:Objekte nicht mehrmals verwerfen")]
+        public static string Encrypt(string plainText, string passPhrase)
+        {
+            if (string.IsNullOrEmpty(passPhrase))
+                throw new ArgumentNullException(nameof(passPhrase));
+
+            // Salt and IV is randomly generated each time, but is preprended to encrypted cipher text
+            // so that the same Salt and IV values can be used when decrypting.
+            var saltStringBytes = GenerateRandomEntropy();
+            var ivStringBytes = GenerateRandomEntropy();
+            var plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+            using (var password = new Rfc2898DeriveBytes(passPhrase, saltStringBytes, DerivationIterations)) {
+                var keyBytes = password.GetBytes(Keysize / 8);
+                using (var symmetricKey = new RijndaelManaged() { BlockSize = BlockSize, Mode = CipherMode.CBC, Padding = PaddingMode.PKCS7 })
+                using (var encryptor = symmetricKey.CreateEncryptor(keyBytes, ivStringBytes))
+                using (var memoryStream = new MemoryStream())
+                using (var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write)) {
+                    cryptoStream.Write(plainTextBytes, 0, plainTextBytes.Length);
+                    cryptoStream.FlushFinalBlock();
+                    // Create the final bytes as a concatenation of the random salt bytes, the random iv bytes and the cipher bytes.
+                    var cipherTextBytes = saltStringBytes
+                        .Concat(ivStringBytes)
+                        .Concat(memoryStream.ToArray()).ToArray();
+                    memoryStream.Close();
+                    cryptoStream.Close();
+                    return Convert.ToBase64String(cipherTextBytes);
+                }
+            }
+        }
+
+        /// <summary>Decrypts the specified cipher text.</summary>
+        /// <param name="cipherText">The cipher text.</param>
+        /// <param name="passPhrase">The pass phrase.</param>
+        /// <returns>The plain text.</returns>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2202:Objekte nicht mehrmals verwerfen")]
+        public static string Decrypt(string cipherText, string passPhrase)
+        {
+            if (string.IsNullOrEmpty(passPhrase))
+                throw new ArgumentNullException(nameof(passPhrase));
+
+            // Get the complete stream of bytes that represent:
+            // [32 bytes of Salt] + [32 bytes of IV] + [n bytes of CipherText]
+            var cipherTextBytesWithSaltAndIv = Convert.FromBase64String(cipherText);
+            // Get the saltbytes by extracting the first 32 bytes from the supplied cipherText bytes.
+            var saltStringBytes = cipherTextBytesWithSaltAndIv.Take(Keysize / 8).ToArray();
+            // Get the IV bytes by extracting the next 32 bytes from the supplied cipherText bytes.
+            var ivStringBytes = cipherTextBytesWithSaltAndIv.Skip(Keysize / 8).Take(Keysize / 8).ToArray();
+            // Get the actual cipher text bytes by removing the first 64 bytes from the cipherText string.
+            var cipherTextBytes = cipherTextBytesWithSaltAndIv.Skip((Keysize / 8) * 2).ToArray();
+
+            using (var password = new Rfc2898DeriveBytes(passPhrase, saltStringBytes, DerivationIterations)) {
+                var keyBytes = password.GetBytes(Keysize / 8);
+                using (var symmetricKey = new RijndaelManaged() { BlockSize = 256, Mode = CipherMode.CBC, Padding = PaddingMode.PKCS7 })
+                using (var decryptor = symmetricKey.CreateDecryptor(keyBytes, ivStringBytes))
+                using (var memoryStream = new MemoryStream(cipherTextBytes))
+                using (var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read)) {
+                    var plainTextBytes = new byte[cipherTextBytes.Length];
+                    var decryptedByteCount = cryptoStream.Read(plainTextBytes, 0, plainTextBytes.Length);
+                    memoryStream.Close();
+                    cryptoStream.Close();
+                    return Encoding.UTF8.GetString(plainTextBytes, 0, decryptedByteCount);
+                }
+            }
+        }
+
+        private static byte[] GenerateRandomEntropy()
+        {
+            var randomBytes = new byte[BlockSize / 8]; // 256 bits in bytes.
+            using (var rngCsp = new RNGCryptoServiceProvider()) {
+                // Fill the array with cryptographically secure random bytes.
+                rngCsp.GetBytes(randomBytes);
+            }
+            return randomBytes;
+        }
+    }
+}
---
+++ CloudFS.AuthenticationTests/StringCipherTests.cs
@@ -0,0 +1,103 @@
+﻿/*
+The MIT License(MIT)
+
+Copyright(c) 2016 IgorSoft
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text.RegularExpressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IgorSoft.CloudFS.Authentication;
+
+namespace IgorSoft.CloudFS.AuthenticationTests
+{
+    [TestClass]
+    public class StringCipherTests
+    {
+        private const string plainText = "PlainText";
+        private const string passPhrase = "PassPhrase";
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Encrypt_WherePassPhraseIsEmpty_Throws()
+        {
+            StringCipher.Encrypt(plainText, string.Empty);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Decrypt_WherePassPhraseIsEmpty_Throws()
+        {
+            StringCipher.Decrypt(plainText, string.Empty);
+        }
+
+        [TestMethod]
+        public void Encrypt_WherePassPhraseIsSet_Succeeds()
+        {
+            var cipherText = StringCipher.Encrypt(plainText, passPhrase);
+
+            Assert.IsNotNull(cipherText, "Encrypted text is null");
+            StringAssert.DoesNotMatch(cipherText, new Regex($".*{plainText}.*"), "Encrypted text contains plain text");
+        }
+
+        [TestMethod]
+        public void Encrypt_WherePassPhraseIsSetAndPlainTextIsEmpty_Succeeds()
+        {
+            var cipherText = StringCipher.Encrypt(string.Empty, passPhrase);
+
+            Assert.IsNotNull(cipherText, "Encrypted text is null");
+        }
+
+        [TestMethod]
+        public void Decrypt_WherePassPhraseMatches_Succeeds()
+        {
+            var cipherText = StringCipher.Encrypt(plainText, passPhrase);
+
+            var decryptedText = StringCipher.Decrypt(cipherText, passPhrase);
+
+            Assert.AreEqual(plainText, decryptedText, "Decrypted text is different from plain text");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CryptographicException))]
+        public void Decrypt_WherePassPhraseDiffers_Fails()
+        {
+            var cipherText = StringCipher.Encrypt(plainText, passPhrase);
+
+            var decryptedText = StringCipher.Decrypt(cipherText, new string(passPhrase.Reverse().ToArray()));
+
+            Assert.AreEqual(plainText, decryptedText, "Decrypted text is different from plain text");
+        }
+
+        [TestMethod]
+        public void Encrypt_WherePassPhraseIsSet_IsSaltedCorrectly()
+        {
+            var cipherText1 = StringCipher.Encrypt(plainText, passPhrase);
+            var cipherText2 = StringCipher.Encrypt(plainText, passPhrase);
+
+            Assert.IsNotNull(cipherText1, "Encrypted text is null");
+            Assert.IsNotNull(cipherText2, "Encrypted text is null");
+            Assert.AreNotEqual(cipherText2, cipherText1, "Encrypted text is not salted");
+        }
+    }
+}
