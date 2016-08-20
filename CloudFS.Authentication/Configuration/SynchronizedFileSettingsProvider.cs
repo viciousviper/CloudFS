a06e8fc4ef96eff3ea4045e43f4205682d0a2116
viciousviper <j_h@mail.org>
2016-08-20 16:16:02 +0200

    Fixed concurrent access to application settings

---
+++ CloudFS.Authentication/Configuration/SynchronizedFileSettingsProvider.cs
@@ -0,0 +1,52 @@
+﻿/*
+The MIT License(MIT)
+
+Copyright(c) 2016 IgorSoft
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+using System;
+using System.Configuration;
+
+namespace IgorSoft.CloudFS.Authentication.Configuration
+{
+    /// <summary>
+    /// Implements synchronized access to a local .config file on top of the <see cref="LocalFileSettingsProvider"/>.
+    /// </summary>
+    /// <seealso cref=LocalFileSettingsProvider" />
+    public class SynchronizedFileSettingsProvider : LocalFileSettingsProvider
+    {
+        private static readonly object lockObject = new object();
+
+        public override SettingsPropertyValueCollection GetPropertyValues(SettingsContext context, SettingsPropertyCollection properties)
+        {
+            lock (lockObject){
+                return base.GetPropertyValues(context, properties);
+            }
+        }
+
+        public override void SetPropertyValues(SettingsContext context, SettingsPropertyValueCollection values)
+        {
+            lock (lockObject) {
+                base.SetPropertyValues(context, values);
+            }
+        }
+    }
+}
